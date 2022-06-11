d99da6d327493cc957be55311d11f178b0a5ba7e
ohadMarmor <ohad@elmar.co.il>
2022-06-12 02:26:32 +0300

    confirm password in Register and let know they don't

--- app/src/main/java/com/example/osapp/Register.java
+++ app/src/main/java/com/example/osapp/Register.java
@@ -2,14 +2,16 @@ package com.example.osapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,8 @@ public class Register extends AppCompatActivity {
                 Intent i = new Intent(this, Chats.class);
                 startActivity(i);
             } else {
+                TextView tv1 = (TextView)findViewById(R.id.textView);
+                tv1.setText("Password don't match");
             }
         });
     }
