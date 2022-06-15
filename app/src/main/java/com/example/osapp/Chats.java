99e1827e2ef50f01a5cd03ecdd9a978bc03c1663
Amit Arie Zakai <amitarie.zakai@live.biu.ac.il>
2022-06-15 13:24:21 +0300

    contacts work

--- app/src/main/java/com/example/osapp/Chats.java
+++ app/src/main/java/com/example/osapp/Chats.java
@@ -21,24 +21,12 @@ import java.util.List;
 import API.ApiContact;
 
 public class Chats extends AppCompatActivity {
-
+//amit
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent i = getIntent();
-        String nickName = i.getStringExtra("nickName");
-        TextView tv = (TextView)findViewById(R.id.nick_name);
-        tv.setText("hello " + nickName);
-
-
-    }
-
-    @SuppressLint("SetTextI18n")
-    @Override
-    protected void onResume() {
-        super.onResume();
         Intent i = getIntent();
         setContentView(R.layout.activity_chats);
 
@@ -48,5 +36,19 @@ public class Chats extends AppCompatActivity {
         lstContacts.setLayoutManager(new LinearLayoutManager(this));
         ApiContact api = new ApiContact();
         api.getAll(i.getStringExtra("userName"), adapter);
+
     }
+
+//    @SuppressLint("SetTextI18n")
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        Intent i = getIntent();
+//        String nickName = i.getStringExtra("nickName");
+//        TextView tv = (TextView)findViewById(R.id.nick_name);
+//        tv.setText("hello " + nickName);
+//
+//
+//    }
 }
