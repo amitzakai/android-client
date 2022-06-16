7f307f3cb8ad6a3b755483d98e94485ae8e6fd7b
Amit Arie Zakai <amitarie.zakai@live.biu.ac.il>
2022-06-16 14:06:13 +0300

    click move to conv design

--- app/src/main/java/com/example/osapp/Conversation.java
+++ app/src/main/java/com/example/osapp/Conversation.java
@@ -2,7 +2,10 @@ package com.example.osapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Conversation extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class Conversation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
+
     }
 }
